8d843efaac6c5117e67c0de7b53c890df681109b
Nephtys <lucie.camerlynck@hotmail.com>
2025-01-31 21:38:48 +0100

    Deleted useless script

--- grafana-templates/sh.sh
+++
@@ -1,4 +0,0 @@
-# Always make a copy before editing with sed
-cp django.json django.json.bac
-# Replaces using regex and saves the file
-sed -i -E 's/"datasource":\s*(".*"|\{[\s\S]*?\})/"datasource": null/g' elasticsearch.json
