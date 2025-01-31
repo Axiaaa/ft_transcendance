7d6070dc2ba5cb2787641b6ecf5a4c12719bfc40
Nephtys <lucie.camerlynck@hotmail.com>
2025-01-31 17:03:26 +0100

    Added grafana and promeheus

--- backend/project/project/urls.py
+++ backend/project/project/urls.py
@@ -20,15 +20,18 @@ from django.urls import path
 from django.urls import include, path
 from rest_framework import routers
 
+from django_prometheus import exports
+
 from tutorial.quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include('django_prometheus.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
