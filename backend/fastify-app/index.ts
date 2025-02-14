22dbb645539cf6828c781d5ef172fb9c70ed3b1b
Nephtys <lucie.camerlynck@hotmail.com>
2025-02-14 14:09:21 +0100

    Fixed DB creation

--- backend/fastify-app/index.ts
+++ backend/fastify-app/index.ts
@@ -4,38 +4,28 @@ import metrics from "fastify-metrics"
 
 
 const Port = process.env.PORT || 4321
-// export const db = new database(`/usr/src/app/db/database.db`)
-export const db = null;
+export const db = new database(`/usr/src/app/db/database.db`)
 
 const server = fastify({
     logger: true
 });
 
-// db.prepare(
-//     `
-//     CREATE TABLE IF NOT EXISTS users (
-//         id INTEGER PRIMARY KEY AUTOINCREMENT,
-//         name TEXT NOT NULL
-//         )
-//         `)
-//         .run();
-        
 
-// server.get('/users', async () => {
-//     return db.prepare('SELECT * FROM users').all();
-// });
+server.get('/users', async () => {
+    return db.prepare('SELECT * FROM users').all();
+});
 
-// server.post<{ Params: { id: string }; Body: { name: string } }>('/users/:id', async (request, reply) => {
-//     const { id } = request.params;
-//     const { name } = request.body;
-//     const stmt = db.prepare('INSERT INTO users (id, name) VALUES (?, ?)');
-//     stmt.run(id, name);
-//     reply.send({ success: true });
-// });
+server.post<{ Params: { id: string }; Body: { name: string } }>('/users/:id', async (request, reply) => {
+    const { id } = request.params;
+    const { name } = request.body;
+    const stmt = db.prepare('INSERT INTO users (id, name) VALUES (?, ?)');
+    stmt.run(id, name);
+    reply.send({ success: true });
+});
 
-// server.get('/ping', async (request, reply) => {
-//     return 'pong\n'
-// })
+server.get('/ping', async (request, reply) => {
+    return 'pong\n'
+})
 
 const start = async () => {
     try {
