749a2f33d90019624db4c7678220cac8fc68fc4b
Nephtys <lucie.camerlynck@hotmail.com>
2025-02-17 15:20:02 +0100

    Added PATCH method

--- backend/fastify-app/index.ts
+++ backend/fastify-app/index.ts
@@ -36,6 +36,37 @@ server.post<{ Body: { name: string, email: string, password: string } }>('/users
         reply.code(409).send({ error: req_message });
 });
 
+server.patch<{
+    Params: { id: string },
+    Body : {
+        username?: string, 
+        email?: string, 
+        password?: string, 
+        is_online?: boolean, 
+        avatar?: string,
+        win_nbr?: number,
+        loss_nbr?: number,
+      }
+    }>('/users/:id', async (request, reply) => {
+
+    const { id } = request.params;
+    const { username, email, password, is_online, avatar, win_nbr, loss_nbr } = request.body;
+    let user = await getUserFromDb(Number(id));
+    if (user == null) {
+        reply.code(404).send({error: "User not found"});
+        return;
+    }
+    if (username) { user.name = username }
+    if (email) { user.email = email }
+    if (password) { user.password = password; }
+    if (is_online) { user.is_online = is_online; }
+    if (avatar) { user.avatar = avatar; }
+    if (win_nbr) { user.win_nbr = win_nbr; }
+    if (loss_nbr) { user.loss_nbr = loss_nbr; }
+    await user.updateUserInDb();
+    reply.code(204).send();
+});
+
 server.get('/ping', async (request, reply) => {
     return 'pong\n'
 })
--- backend/fastify-app/user.ts
+++ backend/fastify-app/user.ts
@@ -37,9 +37,9 @@ export class User implements User {
         this.friend_list = new Array<User>();
     }
 
+    
     async pushUserToDb() : Promise<string | null> {
-        //TODO : Check if the email is already in the DB
-
+        
         if (this.id !== 0) {
             server.log.error(`User ${this.name} already exists in the DB`);
             return "User already exists";
@@ -48,17 +48,17 @@ export class User implements User {
             const insertUser = db.prepare(`
                 INSERT INTO users (username, email, password, is_online, created_at, win_nbr, loss_nbr, avatar)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-            `);
-    
-            const insertFriend = db.prepare(`
-                INSERT INTO friends (user_id, friend_id)
-                VALUES (?, ?)
-            `);
-    
-            db.transaction(() => {
-
-                const result = insertUser.run(
-                    this.name,  
+                `);
+                
+                const insertFriend = db.prepare(`
+                    INSERT INTO friends (user_id, friend_id)
+                    VALUES (?, ?)
+                    `);
+                    
+                    db.transaction(() => {
+                        
+                        const result = insertUser.run(
+                            this.name,  
                     this.email,
                     this.password,
                     this.is_online ? 1 : 0,
@@ -67,7 +67,7 @@ export class User implements User {
                     this.loss_nbr,
                     this.avatar
                 );
-    
+                
                 const lastId = result.lastInsertRowid as number;
                 this.id = lastId;
                 
@@ -79,7 +79,7 @@ export class User implements User {
             })();
             server.log.info(`User ${this.name}, ${this.id} inserted in the DB`);
             return null;
-
+            
         } catch (error) {
             if ((error as any).message.includes("UNIQUE constraint failed: users.email")) {
                 server.log.error(`The email address ${this.email} is already in the DB`);
@@ -89,19 +89,62 @@ export class User implements User {
             return "Error while inserting user in the DB";
         }
     }
-}
 
+    async updateUserInDb() {
+        try {
+            const updateUser = db.prepare(`
+                UPDATE users 
+                SET username = ?, email = ?, password = ?, is_online = ?, win_nbr = ?, loss_nbr = ?, avatar = ?
+                WHERE id = ?
+            `);
+
+            db.transaction(() => {
+                updateUser.run(
+                    this.name,
+                    this.email,
+                    this.password,
+                    this.is_online ? 1 : 0,
+                    this.win_nbr,
+                    this.loss_nbr,
+                    this.avatar,
+                    this.id
+                );
+            })();
+            server.log.info(`User ${this.name}, ${this.id} updated in the DB`);
+        } catch (error) {
+            server.log.error(`Error while updating user ${this.name} in the DB: ${error}`);
+        }
+    }
+}
 
 export async function getUserFromDb(query: number): Promise<User | null> {
 
     try { 
         const sqlrequest = "SELECT * FROM users WHERE id = ?";
-        const user = db.prepare(sqlrequest).get(query) as User | undefined;
-        if (user != undefined)
-            return user;
-        return null;
+        const userRow = db.prepare(sqlrequest).get(query) as { 
+            id: number;
+            username: string;
+            email: string;
+            password: string;
+            is_online: number;
+            created_at: string;
+            win_nbr: number;
+            loss_nbr: number;
+        } | undefined; 
+
+        if (!userRow) return null;
+
+        let user: User = new User(userRow.username, userRow.email, userRow.password);
+        user.id = userRow.id;
+        user.is_online = userRow.is_online === 1;
+        user.created_at = new Date(userRow.created_at);
+        user.win_nbr = userRow.win_nbr;
+        user.loss_nbr = userRow.loss_nbr;
+
+        return user;
     } catch (error) {
         server.log.error(`Could not fetch uer from DB ${error}`)
         return null;
     }
 }
+
