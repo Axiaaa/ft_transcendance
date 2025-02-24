fa82c14539abfa870b901ff05bd9c300d9f4f4b1
Ocyn <niko.ocyn@gmail.com>
2025-02-24 18:09:28 +0100

    Added other profile, working on sign in / sign up part

--- frontend/js/.build/js/login-screen.js
+++ frontend/js/.build/js/login-screen.js
@@ -15,35 +15,41 @@ document.addEventListener('DOMContentLoaded', function () {
     loginScreen.appendChild(loginScreenTopBar);
     loginScreen.appendChild(loginScreenMiddleBar);
     loginScreen.style.display = 'block';
-    var profile = document.getElementsByClassName("login-screen-right-profile-box")[0];
-    var isClicked = false;
-    var isHovered = false;
-    profile.addEventListener('mousedown', function () {
-        isClicked = true;
-        if (isHovered)
-            profile.style.backgroundColor = 'rgba(41, 74, 127, 0.75)';
-        else
+    var profiles = document.getElementsByClassName("login-screen-right-profile-box");
+    var _loop_1 = function (i) {
+        var profile = profiles[i];
+        var isClicked = false;
+        var isHovered = false;
+        profile.addEventListener('mousedown', function () {
+            isClicked = true;
+            if (isHovered)
+                profile.style.backgroundColor = 'rgba(41, 74, 127, 0.75)';
+            else
+                profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
+            loginScreen.style.display = 'none';
             profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
-        loginScreen.style.display = 'none';
-        profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
-        profile.style.backgroundColor = 'transparent';
-    });
-    profile.addEventListener('mouseup', function () {
-        isClicked = false;
-        if (isHovered)
-            profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
-        else
             profile.style.backgroundColor = 'transparent';
-    });
-    profile.addEventListener('mouseenter', function () {
-        isHovered = true;
-        if (isClicked)
-            profile.style.backgroundColor = 'rgba(41, 74, 127, 0.75)';
-        else
-            profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
-    });
-    profile.addEventListener('mouseleave', function () {
-        isHovered = false;
-        profile.style.backgroundColor = 'transparent';
-    });
+        });
+        profile.addEventListener('mouseup', function () {
+            isClicked = false;
+            if (isHovered)
+                profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
+            else
+                profile.style.backgroundColor = 'transparent';
+        });
+        profile.addEventListener('mouseenter', function () {
+            isHovered = true;
+            if (isClicked)
+                profile.style.backgroundColor = 'rgba(41, 74, 127, 0.75)';
+            else
+                profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
+        });
+        profile.addEventListener('mouseleave', function () {
+            isHovered = false;
+            profile.style.backgroundColor = 'transparent';
+        });
+    };
+    for (var i = 0; i < profiles.length; i++) {
+        _loop_1(i);
+    }
 });
--- frontend/js/login-screen.ts
+++ frontend/js/login-screen.ts
@@ -39,38 +39,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	loginScreen.style.display = 'block';
 
-	let profile = document.getElementsByClassName("login-screen-right-profile-box")[0] as HTMLElement;
+	let profiles = document.getElementsByClassName("login-screen-right-profile-box") as HTMLCollectionOf<HTMLElement>;
 
-	let isClicked = false;
-	let isHovered = false;
-	profile.addEventListener('mousedown', () => {
-		isClicked = true;
-		if (isHovered)
-			profile.style.backgroundColor = 'rgba(41, 74, 127, 0.75)';
-		else
+	for (let i = 0; i < profiles.length; i++) {
+		let profile = profiles[i];
+		let isClicked = false;
+		let isHovered = false;
+		profile.addEventListener('mousedown', () => {
+			isClicked = true;
+			if (isHovered)
+				profile.style.backgroundColor = 'rgba(41, 74, 127, 0.75)';
+			else
+				profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
+			
+			loginScreen.style.display = 'none';
 			profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
-		
-		loginScreen.style.display = 'none';
-		profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
-		profile.style.backgroundColor = 'transparent';
-	});
-	profile.addEventListener('mouseup', () => {
-		isClicked = false;
-		if (isHovered)
-			profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
-		else
 			profile.style.backgroundColor = 'transparent';
-	});
-	profile.addEventListener('mouseenter', () => {
-		isHovered = true;
-		if (isClicked)
-			profile.style.backgroundColor = 'rgba(41, 74, 127, 0.75)';
-		else
-			profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
-	});
-	profile.addEventListener('mouseleave', () => {
-		isHovered = false;
-		profile.style.backgroundColor = 'transparent';
+		});
+		profile.addEventListener('mouseup', () => {
+			isClicked = false;
+			if (isHovered)
+				profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
+			else
+				profile.style.backgroundColor = 'transparent';
+		});
+		profile.addEventListener('mouseenter', () => {
+			isHovered = true;
+			if (isClicked)
+				profile.style.backgroundColor = 'rgba(41, 74, 127, 0.75)';
+			else
+				profile.style.backgroundColor = 'rgba(59, 104, 178, 0.61)';
+		});
+		profile.addEventListener('mouseleave', () => {
+			isHovered = false;
+			profile.style.backgroundColor = 'transparent';
+		});
 	}
-	);
+
+	
 });
