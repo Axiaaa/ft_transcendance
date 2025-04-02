b3d591826ae85ca3505073069ea0b2def1ce31e0
nonomex <nonomex175@gmail.com>
2025-04-02 15:13:35 +0200

    trying to make api works, cleaning same name variable

--- frontend/js/.build/js/api.js
+++ frontend/js/.build/js/api.js
@@ -1,8 +1,8 @@
-export async function login(username, password) {
-    const loginData = { username, password };
+export async function login(signin_username, signin_password) {
+    const loginData = { signin_username, signin_password };
   
     try {
-      const response = await fetch('/https://localhost/api/users/${username}', {
+      const response = await fetch('/https//localhost/api/users/${username}', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -32,22 +32,22 @@ export async function login(username, password) {
 document.getElementById('loginform').addEventListener('submit', async function(event) {
 event.preventDefault();
   
-const username = document.getElementById('username').value;
-const password = document.getElementById('password').value;
+const signin_username = document.getElementById('username').value;
+const signin_password = document.getElementById('password').value;
 
 try {
-    const loginResult = await login(username, password);
+    const loginResult = await login(signin_username, signin_password);
     console.log('Login result:', loginResult);
     window.location.href = '/dashboard'; 
 } catch (err) {
     alert('Login failed: ' + err.message);
 }});
 
-export async function signup(username, password) {
-  const signupData = { username, password };
+export async function signup(signup_username, signup_password) {
+  const signupData = { signup_username, signup_password };
 
   try {
-      const response = await fetch('https://localhost/api', {
+      const response = await fetch('https://localhost/api/users', {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json',
@@ -78,11 +78,11 @@ export async function signup(username, password) {
 document.getElementById('signupform').addEventListener('submit', async function(event) {
   event.preventDefault();
 
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  const signup_username = document.getElementById('newUsername').value;
+  const signup_password = document.getElementById('newPassword').value;
 
   try {
-      const signupResult = await signup(username, password);
+      const signupResult = await signup(signup_username, signup_password);
       console.log('Signup result:', signupResult);
       window.location.href = '/dashboard'; 
   } catch (err) {
