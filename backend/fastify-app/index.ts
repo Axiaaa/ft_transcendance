709463dce641b383f2cf6ff063b27dbac6a22eb3
nono <fsd>
2025-04-11 15:46:46 +0200

    wrong branchech

--- backend/fastify-app/index.ts
+++ backend/fastify-app/index.ts
@@ -4,6 +4,7 @@ import metrics from "fastify-metrics"
 import { userRoutes } from "./routes/user";
 import { tournamentRoutes } from "./routes/tournaments";
 import { matchsRoutes } from "./routes/matchs";
+import rateLimit from '@fastify/rate-limit';
 
 const Port = process.env.PORT || 4321
 const envUser = process.env.API_USERNAME || 'admin'
@@ -14,19 +15,40 @@ export const server = fastify({
     logger: true
 });
 
+
+
+// const registerRateLimit = async () => {
+//   await server.register(rateLimit, {
+//     max: 10, // ⏱ 60 requests per...
+//     timeWindow: '1 minute', // ...per minute per IP
+//     addHeaders: {
+//       'x-ratelimit-limit': true,
+//       'x-ratelimit-remaining': true,
+//       'x-ratelimit-reset': true,
+//     },
+//     errorResponseBuilder: () => {
+//       return {
+  //         statusCode: 429,
+  //         error: 'Too Many Requests',
+  //         message: 'Vous avez dépassé la limite de requêtes (60/minute). Réessayez plus tard.',
+  //       };
+  //     },
+  //   });
+  // };
+
 server.addHook('preHandler', async (req, reply) => {
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Basic ')) {
       return reply.code(401).send({ error: 'Unauthorized' });
     }
   
-    const base64Credentials = authHeader.split(' ')[1];
-    const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
-    const [username, password] = credentials.split(':');
+    // const base64Credentials = authHeader.split(' ')[1];
+    // const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+    // const [username, password] = credentials.split(':');
   
-    if (username !== envUser || password !== envPassword) {
-      return reply.code(401).send({ error: 'Unauthorized. Please provid valid credentials' });
-    }
+    // if (username !== envUser || password !== envPassword) {
+    //   return reply.code(401).send({ error: 'Unauthorized. Please provid valid credentials' });
+    // }
 
     if (req.method === 'POST' || req.method === 'PATCH') {
       try {
@@ -44,6 +66,10 @@ server.get('/ping', async (request, reply) => {
 
 const start = async () => {
     try {
+        //await registerRateLimit();
+        await server.register(rateLimit, {
+          global: false, // Global rate limiting is disabled so we can set it route by route
+        });
         await server.register(metrics,{endpoint: '/metrics'});
         await server.register(tournamentRoutes, { prefix: '/api' });
         await server.register(userRoutes, { prefix: '/api' });
@@ -55,6 +81,7 @@ const start = async () => {
         process.exit(1)
     }
 }
+
 
 start();
 
