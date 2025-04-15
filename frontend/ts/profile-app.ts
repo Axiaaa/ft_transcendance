9f408f78847837c1463856e4a37438f8e2108e45
Nephtys <lucie.camerlynck@hotmail.com>
2025-04-15 22:23:02 +0200

    Added username fetching, still need some work

--- frontend/ts/profile-app.ts
+++ frontend/ts/profile-app.ts
@@ -1,5 +1,6 @@
 import { openAppWindow } from "./app-icon.js";
 import { sendNotification } from "./notification.js";
+import { getCurrentUser } from "./API.js";
 
 function openProfile(AppLauncher: string, profileTab?: string): void
 {
@@ -240,7 +241,7 @@ function addTournamentHistory(Container: HTMLElement, Player1: string, Player2: 
 }
 
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
 
 	let App = document.getElementById('profile-app-window') as HTMLElement;
 	if (!App) return;
@@ -356,10 +357,19 @@ document.addEventListener('DOMContentLoaded', () => {
 					avatarImg.style.objectFit = 'cover';
 					avatarContainer.appendChild(avatarImg);
 
-					// Username display
-					// API CALL NEEDED: Fetch user's username from the backend
-					let usernameDisplay = document.createElement('h3');
-					usernameDisplay.innerText = 'Xxx_D4rkS4suke36_xxX'; // Default Username
+					let usernameDisplay = document.createElement('h2');
+					usernameDisplay.innerText = 'Loading...'; // Placeholder text while fetching username
+					try {
+						console.log('Fetching username...');
+						const user = await getCurrentUser(sessionStorage.getItem('wxp_token'));
+						if (user)
+							usernameDisplay.innerText = user.username;
+					}
+					catch (error)
+					{
+						console.error('Error fetching username:', error);
+						usernameDisplay.innerText = 'Unknown User, please log in';
+					}
 					usernameDisplay.style.color = '#333';
 					usernameDisplay.style.fontSize = '18px';
 					usernameDisplay.style.fontWeight = 'bold';
